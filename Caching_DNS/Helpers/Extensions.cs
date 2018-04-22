73b88420387143339963cc350830409f29ddd470
Ollisteka <zhukova.o.m@yandex.ru>
2018-04-22 21:35:55 +0500

    Исправила NullReferenceException

--- Caching_DNS/Helpers/Extensions.cs
+++ Caching_DNS/Helpers/Extensions.cs
@@ -31,8 +31,8 @@ namespace Caching_DNS.Helpers
         public static string GetCustomDescription(object objEnum)
         {
             var fi = objEnum.GetType().GetField(objEnum.ToString());
-            var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
-            return (attributes.Length > 0) ? attributes[0].Description : objEnum.ToString();
+            var attributes = (DescriptionAttribute[])fi?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return (attributes?.Length > 0) ? attributes[0].Description : objEnum.ToString();
         }
 
         public static string Description(this Enum value)
