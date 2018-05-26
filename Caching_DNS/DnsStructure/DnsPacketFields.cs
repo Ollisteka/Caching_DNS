ce424f4b23b9679a5a52f950ebce7e0968d816c2
Olga Zhukova <zhukova.o.m@yandex.ru>
2018-05-26 10:58:06 +0500

    static -> const

--- Caching_DNS/DnsStructure/DnsPacketFields.cs
+++ Caching_DNS/DnsStructure/DnsPacketFields.cs
@@ -2,17 +2,17 @@
 {
     public static class DnsPacketFields
     {
-        public static int TransactionId = 0;
-        public static int Flags = 2;
-        public static int Questions = 4;
-        public static int Answers = 6;
-        public static int Authority = 8;
-        public static int Additional = 10;
-        public static int Queries = 12;
+        public const int TransactionId = 0;
+        public const int Flags = 2;
+        public const int Questions = 4;
+        public const int Answers = 6;
+        public const int Authority = 8;
+        public const int Additional = 10;
+        public const int Queries = 12;
 
-        public static uint AuthorativeAnswerMask = 0b0000_0100_0000_0000;
-        public static uint TruncatedMask = 0b0000_0010_0000_0000;
-        public static uint RecursionDesiredMask = 0b0000_0001_0000_0000;
-        public static uint RecursionAvailableMask = 0b0000_0000_1000_0000;
+        public const uint AuthorativeAnswerMask = 0b0000_0100_0000_0000;
+        public const uint TruncatedMask = 0b0000_0010_0000_0000;
+        public const uint RecursionDesiredMask = 0b0000_0001_0000_0000;
+        public const uint RecursionAvailableMask = 0b0000_0000_1000_0000;
     }
 }
